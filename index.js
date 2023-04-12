4cd3978897d102f7b00a2202f76d1b350b7a0a58
Eli Friedlander <eli.friedlander@gmail.com>
2023-04-12 13:37:41 -0400

    committing

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    const cool = document.getElementById('text');
+    console.log(cool)
+    cool.textContent = "This is really cool!"
+}
+)
